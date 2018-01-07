73f57fb275212ef5fe75a4e12b13faed5edcdcb1
Rui <rui@dhcp154.merusouth116.iit.edu>
2018-01-06 23:43:11 -0600

    first commit

--- LeetCode139_WordBreak/main.cpp
+++ LeetCode139_WordBreak/main.cpp
@@ -13,3 +13,26 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int length = s.size();
+        vector<bool> res(length + 1, false);
+        res[0] = true;
+        
+        for(int i = 0; i < length + 1; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(res[j] && wordDict.find(wordDict.begin(), wordDict.end(), s.substr(j, i-j)) != wordDict.end())
+                {
+                    res[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return res[length];
+    }
+};
